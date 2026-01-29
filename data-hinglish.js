54169eff11b8e867b242bfb24ec046e09ba98adf
Kostov Phuk <moinaphukon2@gmail.com>
2026-01-29 19:31:58 +0530

    Create data-hinglish.js

---
+++ data-hinglish.js
@@ -0,0 +1,21 @@
+const stories_hinglish = [
+
+  {
+    title: "The Lazy Student 😂",
+    content: `Ravi studied only 5 minutes before exam.
+But still he said —
+"Bhagwan bharose!" 😆
+
+Result day...
+Bhagwan also said —
+"Tu khud dekh le!"`
+  },
+
+  {
+    title: "Free WiFi Love ❤️",
+    content: `She came for free WiFi…
+He came for tea…
+Now both come daily… only to see each other 😄`
+  }
+
+];
