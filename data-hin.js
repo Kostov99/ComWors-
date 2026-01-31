4638542cb4d0020dc96667427b612e701d6e6092
Kostov Phuk <moinaphukon2@gmail.com>
2026-01-31 20:50:34 +0530

    Update and rename data-hinglish.js to data-hin.js

---
+++ data-hin.js
@@ -0,0 +1,18 @@
+const stories_hin = [
+{
+title: "Exam ka Hero 😂",
+content: `Ravi ne pura saal PUBG khela.
+Exam ke din bola —
+"Bhagwan sambhal lena!"
+
+Result aaya...
+Bhagwan bola —
+"Beta ab tu hi sambhal!" 😆`
+},
+{
+title: "Free WiFi Love ❤️",
+content: `Woh WiFi ke liye aayi…
+Main chai ke liye…
+Ab dono roz aate hai… ek dusre ke liye 😄`
+}
+];
